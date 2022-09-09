6df9bdcfc6e14da8c48a728defe38e275e5898e5
Yoepee <kdy2975@naver.com>
2022-09-09 12:09:38 +0900

    리덕스 컴포넌트 페이지 구조 설계

---
+++ src/redux/store.js
@@ -0,0 +1,17 @@
+import { configureStore} from "@reduxjs/toolkit";
+import user from "./modules/user"
+
+import post from "./modules/post"
+import detailPost from "./modules/detailPost"
+
+import comment from "./modules/commnet"
+
+import detailThing from "./modules/detailThing"
+import thing from "./modules/thing"
+
+
+export default configureStore({
+    reducer: {
+      
+    }
+});
---
+++ src/shared/Router.jsx
@@ -0,0 +1,11 @@
+import {Route, Routes} from "react-router-dom"
+
+const Router = () => {
+    return  (
+        <div>
+            헬로
+        </div>
+    )
+}
+
+export default Router;
