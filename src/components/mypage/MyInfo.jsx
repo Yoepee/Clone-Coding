6e9e041f9d04be4a2d534d652aaa418aaa27a3f7
Yoepee <kdy2975@naver.com>
2022-09-11 15:28:55 +0900

    로그인 회원가입 기능구현

--- src/components/mypage/MyInfo.jsx
+++ src/components/mypage/MyInfo.jsx
@@ -4,6 +4,7 @@ import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ShoppingBagRoundedIcon from '@mui/icons-material/ShoppingBagRounded';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ReceiptIcon from '@mui/icons-material/Receipt';
+import bg from "../../img/당근페이.png"
 const MyInfo = () => {
     const navigate = useNavigate();
     return (
@@ -24,7 +25,7 @@ const MyInfo = () => {
             border: "2px dashed #ff5722",
             borderRadius:"10px"
             }}>
-                <div>당근페이</div>
+                <LogoImg src={bg}/>
                 <p style={{marginLeft:"auto", marginRight:"10px"}}>중고거래는 이제 당근페이로 해보세요!</p>
             </div>
             <BtnSet>
@@ -98,4 +99,11 @@ width: 70px;
 height: 70px;
 color:#ff5722;
 cursor:pointer;
+`
+
+
+const LogoImg = styled.img`
+width : 110px;
+height: 50px;
+margin-left : 5px;
 `
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,55 +1,15 @@
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import axios from "axios"
-
 // 회원가입 페이지
-const SignUpPage = () => {
-    let navigate = useNavigate();
-    let dispatch = useDispatch();
-    let initialState = {
-        phoneNumber: "",
-        passWord: "",
-        nickname:""
-    }
-    let [member, setMember] = useState(initialState);
-    const onChangeHandler = (event) => {
-        // name과 value를 input의 event.target으로 지정 (파라미터 참조)
-        const { name, value } = event.target;
-        // 수정되는 내용과 member이 가진 값을 매칭하여 state변경
-        setMember({ ...member, [name]: value });
-      };
 
-      const onClickHandler = (event) => {
 
-      }
+import SignUp from "../components/signuppage/SignUp";
+
+
+const SignupPages = () => {
     return  (
         <div>
-            <label>
-            <input placeholder="휴대폰 번호"
-                    onChange={onChangeHandler}
-                    name="phoneNumber"
-                    value={member.phoneNumber}
-                    type="text"/>
-                    <button>휴대폰 중복검사</button>
-            </label>
-            <input placeholder="비밀번호"
-                    onChange={onChangeHandler}
-                    name="passWord"
-                    value={member.passWord}
-                    type="password"/>
-            <label>
-            <input placeholder="닉네임"
-                    onChange={onChangeHandler}
-                    name="nickname"
-                    value={member.nickname}
-                    type="text"/>
-                    <button>닉네임 중복검사</button>
-            </label>
-            <button>회원가입</button>
-            <button onClick={()=>{navigate("/login")}}>취소</button>
+            <SignUp />
         </div>
     )
 }
 
-export default SignUpPage;
+export default SignupPages;
