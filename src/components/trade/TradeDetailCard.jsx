c92495927a234eaeb13ea70bd77cfe68af81bbf3
yesol_lee <thfl3363@hanmail.net>
2022-09-11 20:59:13 +0900

    post view 완성

--- src/components/trade/TradeDetailCard.jsx
+++ src/components/trade/TradeDetailCard.jsx
@@ -4,37 +4,52 @@ import React from "react";
 import styled from "styled-components";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import SellerCard from "./SellerCard";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { useNavigate } from "react-router-dom";
 import RelationCard from "./RelationCard";
-import HomeIcon from '@mui/icons-material/Home';
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import HomeIcon from "@mui/icons-material/Home";
+import ShareIcon from "@mui/icons-material/Share";
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 
 const TradeDetailCard = () => {
-  
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <div>
-      <div><ArrowBackIcon
+      <MenuContainer>
+
+        <First >
+          <ArrowBackIcon
+           sx={{ color: "#ffffff",fontSize: 30 }} 
+          onClick={() => {
+            navigate(-1);
+          }}
+        />
+        <HomeIcon
+        sx={{ color: "#ffffff",fontSize: 30 }}
           onClick={() => {
             navigate("/");
           }}
         />
-        <HomeIcon onClick={() => {
-            navigate("/");
-          }} /></div>
+        </First>
        
+        <Second>
+        <ShareIcon sx={{ color: "#ffffff",fontSize: 30 }}
+        />
+        <MenuRoundedIcon sx={{ color: "#ffffff",fontSize: 30 }}/>
+        </Second >
+      </MenuContainer>
+
       <img
-        style={{ maxWidth: "100%", height: "auto" }}
+        style={{width:"100%", height: "auto"}}
         src="https://blog.kakaocdn.net/dn/uneoL/btryc7LVoLk/VuscYaSvbQRfhgP42hsv0k/img.jpg"
       ></img>
-      <Container
-        style={{ display: "flex", flexDirection: "row"}}
-      >
+
+      <Container style={{ display: "flex", flexDirection: "row" }}>
         <AccountCircleIcon
           style={{ width: "50px", height: "50px", marginRight: "10px" }}
         />
-        <div style={{ flexDitrection: "colummn"}}>
+        <div style={{ flexDitrection: "colummn" }}>
           <div style={{ fontWeight: "bolder", fontSize: "18px" }}>영희</div>
           <div>원주 태장1동</div>
         </div>
@@ -49,18 +64,23 @@ const TradeDetailCard = () => {
         <br />
         <div>내용</div>
         <br />
-        <div>관심 1  조회 6</div>
-        <br/>
+        <div>관심 1 조회 6</div>
+        <br />
       </Container>
-
-      <Container>이 게시글 신고하기</Container>
-      <Container >
-        <div style={{ fontWeight: "bolder", fontSize: "19px"}}>OO님의 판매상품</div>
+      <Container style={{fontWeight:"600", justifyContent:"center"}}>
+        이 게시글 신고하기
+        </Container>
+      <Container>
+        <div style={{ fontWeight: "bolder", fontSize: "19px" }}>
+          OO님의 판매상품
+        </div>
         <SellerCard />
       </Container>
 
       <Container>
-      <div style={{ fontWeight: "bolder", fontSize: "19px"}}>OO님, 이건 어때요?</div>
+        <div style={{ fontWeight: "bolder", fontSize: "19px" }}>
+          OO님, 이건 어때요?
+        </div>
         <RelationCard />
       </Container>
     </div>
@@ -76,3 +96,29 @@ const Container = styled.div`
   min-height: 60px;
   margin: 20px;
 `;
+
+const MenuContainer = styled.div`
+ /* position: absolute; */
+  /* display:grid;
+  grid-template-rows: 1fr 1fr;
+  grid-template-areas: 
+  'first second'; */
+  
+`
+const First = styled.div`
+  /* grid-area: first;
+  float: left; */
+  position: absolute;
+  left: 0;
+  top: 0;
+  display: flex;
+`
+
+const Second = styled.div`
+  /* grid-area: second;
+  float: right; */
+  position: absolute;
+  right: 0;
+  top: 0;
+  display: flex;
+`
